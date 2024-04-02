446e234caf587a09ef5585851ff17413cca3bb45
CantarinoG <guilhermecantarino1709@gmail.com>
2024-04-02 14:54:49 -0300

    init: Add initial project structure

---
+++ script.js
@@ -0,0 +1,12 @@
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
+animate();
